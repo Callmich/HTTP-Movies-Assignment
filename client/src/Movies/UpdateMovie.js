a9fbf7cbe5308122fef96fcbe39bb71ffd243f5d
Michael Callahan <CallahanMichaelE@gmail.com>
2020-03-25 15:56:37 -0600

    added guts of the update movie component. When it renders it now shows the form as well as the previous data from the movie to be edited

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,12 +1,85 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import axios from 'axios';
+import Movie from './Movie';
 
-const UpdateMovie = () => {
+const initialMovie = {
+    id: '',
+    title: '',
+    director: '',
+    metascore: '',
+    stars: []
+  }
 
+const UpdateMovie = props => {
 
+    const { id } = useParams();
+    const { push } = useHistory();
+    const [movie, setMovie] = useState(initialMovie)
+
+    const changeHandler = ev => {
+        ev.persist();
+        let value = ev.target.value;
+        if (ev.target.name === 'metascore') {
+          value = parseInt(value, 10);
+        }
+
+        setMovie({
+            ...movie,
+            [ev.target.name]: value
+        })
+    }
+
+    useEffect(()=>{
+        const movieToUpdate = props.movies.find(element => `${element.id}` === id)
+        console.log("PROPS in mvoie",props)
+        if (movieToUpdate) {
+            setMovie(movieToUpdate)
+        }
+    }, [props.movies, id])
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+
+    }
 
     return(
-        <h2>Update Movie</h2>
+        <div>
+          <h2>Update Movie</h2>
+          <form onSubmit={handleSubmit}>
+            <input 
+              type="text"
+              name="title"
+              onChange={changeHandler}
+              placeholder="Title"
+              value={movie.title}
+            />
+            <input 
+              type="text"
+              name="director"
+              onChange={changeHandler}
+              placeholder="Director"
+              value={movie.director}
+            />
+            <input 
+              type="text"
+              name="metascore"
+              onChange={changeHandler}
+              placeholder="Metascore"
+              value={movie.metascore}
+            />
+            <input 
+              type="text"
+              name="stars"
+              onChange={changeHandler}
+              placeholder="Stars"
+              value={movie.stars}
+            />
+            <button>Update</button>
+          </form>
+        </div>
     )
 }
+
 
 export default UpdateMovie
